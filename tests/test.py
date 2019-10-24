7459d40f0ab9e4b8b69ca7aca759e05e29737850
Fang <saij84@gmail.com>
2019-10-24 22:44:11 +1100

    started the extension for phase two

--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,16 @@
-from mediaRename.utils import utils
 from mediaRename.constants import constants as CONST
 from mediaRename.core import tree2Json as t2j
 from pprint import pprint
 import json, re
+from mediaRename.utils import utils
 
 jsonFilePath = "C:\\NAS\\Anime\\data.json"
 cleanStart = re.compile(CONST.RE_REPLACE)
+arr = list()
 
 with open(jsonFilePath) as json_file:
     data = json.load(json_file)
-
-    for p in data['newName']:
-        pprint(cleanStart.findall(p))
-    #     print('Name: ' + p['name'])
-    #     print('Website: ' + p['website'])
-    #     print('From: ' + p['from'])
-    #     print('')
+    # extractData = utils.extractValues(data, arr, "newName")
+    # pprint(extractData)
+    utils.changeValues(data, "newName")
+    print(data)
--- utils/utils.py
+++ utils/utils.py
@@ -6,3 +6,30 @@ def toFile(inPath, dataDump, outFile="data"):
 
 def getFileExtension(inputText):
     return inputText.split(".")[-1]
+
+def extractValues(obj, valueArr, key):
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            if isinstance(v, (dict, list)):
+                extractValues(v, valueArr, key)
+            elif k == key:
+                valueArr.append(v)
+    elif isinstance(obj, list):
+        for item in obj:
+            extractValues(item, valueArr, key)
+    return valueArr
+
+def changeValues(obj, key):
+    if isinstance(obj, dict):
+        for dictKey, dictVal in obj.items():
+            if isinstance(dictVal, (dict, list)):
+                changeValues(dictVal, key)
+            elif dictKey == key:
+                #print(dictKey, dictVal)
+                obj[dictKey] = dictVal + "_TEST"
+                print(obj)
+
+    elif isinstance(obj, list):
+        for item in obj:
+            changeValues(item, key)
+
